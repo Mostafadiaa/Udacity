41ec75d60d7e1bc167009af20b10a6894583920f
Mostafa Diaa <moustafa.diaa1996@gmail.com>
2019-01-29 19:09:46 +0200

    new task

---
+++ dog Api udacity task/dog Api udacity task/Model/dogImage.swift
@@ -0,0 +1,14 @@
+//
+//  dogImage.swift
+//  dog Api udacity task
+//
+//  Created by Mostafa Diaa on 1/28/19.
+//  Copyright © 2019 Mostafa Diaa. All rights reserved.
+//
+
+import Foundation
+struct dogImage:Codable {
+    let status:String
+    let message:String
+    
+}
